a66bd55d841db1315ec37c8371bf17d7b595eb9b
williamsb8 <39642075+williamsb8@users.noreply.github.com>
2023-06-21 17:57:58 -0500

    Implementacion del paquete UUID y el servicio.

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -11,9 +11,11 @@ export class ListComponent {
   public characterList: Character[] = [];
 
   @Output()
-  public onDelete: EventEmitter<number> = new EventEmitter();
+  public onDelete: EventEmitter<string> = new EventEmitter();
 
-  onDeleteCharacter(index: number): void {
-    this.onDelete.emit(index);
+  onDeleteCharacter(id?: string): void {
+    if (!id) return;
+    //console.log(id);
+    this.onDelete.emit(id);
   }
 }
